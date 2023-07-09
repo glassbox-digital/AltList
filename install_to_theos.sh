36ac23a48e0b4386fcca449513851d686f4f1141
AviGelkop <avi.gelkop@glassboxdigital.com>
2023-07-09 18:38:44 +0300

    Update install_to_theos.sh

--- install_to_theos.sh
+++ install_to_theos.sh
@@ -4,8 +4,8 @@ make FINALPACKAGE=1
 cp -Rv "./.theos/obj/AltList.framework" "$THEOS/lib"
 
 make clean
-make FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless
 mkdir -p "$THEOS/lib/iphone/rootless/lib"
+make FINALPACKAGE=1 THEOS_PACKAGE_SCHEME=rootless
 cp -Rv "./.theos/obj/AltList.framework" "$THEOS/lib/iphone/rootless"
 
 echo "Successfully installed AltList"
